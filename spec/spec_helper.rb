f83191d0e17cb9006f7b51d9cc28030db61c4db1
Joe Fraiman <joe.fraiman@gmail.com>
2011-10-28 16:55:42 -0700

    Done with sign in

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,17 +14,14 @@ Spork.prefork do
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
   RSpec.configure do |config|
-    # == Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
-    config.mock_with :rspec
 
+    config.mock_with :rspec
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+    def test_sign_in(user)
+      controller.sign_in(user)
+    end
+    
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, comment the following line or assign false
     # instead of true.
